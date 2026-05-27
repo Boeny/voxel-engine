75e756a6e430c8f9518a4cd07fb7026caffb08e3
aleksandr.sychev <aleksandr.sychev@nordigy.ru>
2026-05-27 20:23:34 +0400

    fix bloom artefacts

--- src/core/components/StarField/const.ts
+++ src/core/components/StarField/const.ts
@@ -7,9 +7,9 @@ import { StarShaderParams } from './types';
 export const STAR_SHADER_PARAMS: StarShaderParams = {
   uBrightnessMultiplier: 1e13,
   uRadiusMultiplier: 1,
-  uMinRadius: 4,
+  uMinRadius: 3,
   uMinBrightness: 0,
-  uMaxBrightness: 10,
+  uMaxBrightness: 3,
   uCameraPositionLy: new Vector3(),
   uPixelAngularSize: 0,
   LY_TO_KM,
